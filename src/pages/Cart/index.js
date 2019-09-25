0ed9c631918e0536dba000cea009361e99ebdc2c
Fernando Augusto <fernandoajneto@gmail.com>
2019-09-25 02:52:55 -0300

    Styling cart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>Name</th>
+            <th>Qtd</th>
+            <th>Subtotal</th>
+            <th />
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-vr-caminhada-confortavel-detalhes-couro-masculino/04/E74-0413-304/E74-0413-304_detalhe2.jpg?ims=326x"
+                alt="Tenis"
+              />
+            </td>
+            <td>
+              <strong>Tenis massa</strong>
+              <span>129.90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159C1" />
+                </button>
+                <input type="number" readOnly value={1} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159C1" />
+                </button>
+              </div>
+            </td>
+
+            <td>
+              <strong>R$258.80</strong>
+            </td>
+
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159C1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finish order</button>
+
+        <Total>
+          <span>Total</span>
+          <strong>R$199.99</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
